a2875d7e792db94e6040abf4d73376eaeff4dd17
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-02-25 18:11:38 +0530

    fetching Api data and showing Retryig error

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
 import "./App.css";
 import MovieList from "./component/MovieList";
-import ReactLoading from "react-loading";
 import { useEffect, useState } from "react";
+import Movie from "./component/Movie";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+
   async function fetchMoviehandler() {
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
     setIsloading(true);
-    setTimeout(() => {
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/fils");
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+
+      const data = await response.json();
+
       setMovies(data.results);
-      setIsloading(false);
-    }, 3000);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsloading(false);
+  }
+
+  //eligant way to write code
+  let content = <p>No Movies Found</p>;
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        {isLoading ? (
-          <ReactLoading />
-        ) : (
+        <section>
           <button className="fetchmovieButton" onClick={fetchMoviehandler}>
             Fetch Movie
           </button>
-        )}
-        <section>
-          <MovieList movies={movies} />
         </section>
+        <section>{content}</section>
       </header>
     </div>
   );
